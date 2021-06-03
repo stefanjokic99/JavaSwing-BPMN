ceeb857c308689206c992223d8db3364360aeef5
StefanJ2022 <StefanJ2022@66b613c1-3a70-904c-bb12-c541796278d3>
2021-06-03 13:00:13 +0000

    MainFrame and MainPanel commit

--- G1/Projekat/trunk/Empresa(prototip)/src/main/MainClass.java
+++ G1/Projekat/trunk/Empresa(prototip)/src/main/MainClass.java
@@ -1,10 +1,11 @@
 package main;
 
+import frame.MainFrame;
+
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MainFrame();
 	}
 
 }
