665519b574606cfbe424c1532a379a98ee14c625
StefanJ2022 <StefanJ2022@66b613c1-3a70-904c-bb12-c541796278d3>
2021-06-09 18:27:18 +0000

    dodat mehanizam izlaska, window listner i general helper(Metoda za ikonice)

---
+++ G1/Projekat/trunk/Empresa(prototip)/src/helpers/GeneralHelper.java
@@ -0,0 +1,15 @@
+package helpers;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class GeneralHelper {
+
+	public static Icon getIconFromName(String iconName) {
+		Image iconImage = Toolkit.getDefaultToolkit().getImage("icons/" + iconName +  ".png");
+		return new ImageIcon(iconImage);
+	}
+}
