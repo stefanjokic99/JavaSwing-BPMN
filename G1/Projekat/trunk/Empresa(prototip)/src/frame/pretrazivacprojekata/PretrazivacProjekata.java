b5bc09a77968c84bb2b00849afad6720e0845dc0
StefanJ2022 <StefanJ2022@66b613c1-3a70-904c-bb12-c541796278d3>
2021-06-19 11:18:20 +0000

    :D

--- G1/Projekat/trunk/Empresa(prototip)/src/frame/pretrazivacprojekata/PretrazivacProjekata.java
+++ G1/Projekat/trunk/Empresa(prototip)/src/frame/pretrazivacprojekata/PretrazivacProjekata.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
+import helpers.GeneralHelper;
+
 public class PretrazivacProjekata extends JTabbedPane {
 
 	/**
@@ -18,8 +20,8 @@ public class PretrazivacProjekata extends JTabbedPane {
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);  
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
         
-		addTab("Projekat", scrollPane);
-		addTab("Repozitorijum", new PanelRepozitorijum());
+		addTab("Projekat", GeneralHelper.getIconFromName("package"), scrollPane);
+		addTab("Repozitorijum", GeneralHelper.getIconFromName("drive_network"), new PanelRepozitorijum());
 		setPreferredSize(new Dimension(200, 0));
 		setTabPlacement(JTabbedPane.BOTTOM);
 	}
