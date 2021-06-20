d0dd2fe21be5c2372e33dd4f66aee531d3c971ee
StefanJ2022 <StefanJ2022@66b613c1-3a70-904c-bb12-c541796278d3>
2021-06-20 17:45:39 +0000

    helper panel i centrirani naslovi u paleti alatki

--- G1/Projekat/trunk/Empresa(prototip)/src/helpers/GeneralHelper.java
+++ G1/Projekat/trunk/Empresa(prototip)/src/helpers/GeneralHelper.java
@@ -5,6 +5,8 @@ import java.awt.Toolkit;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GeneralHelper {
 
@@ -19,5 +21,10 @@ public class GeneralHelper {
 	public static Icon getElementIconFromName(String iconName) {
 		Image iconImage = Toolkit.getDefaultToolkit().getImage("images/elements/" + iconName +  ".png");
 		return new ImageIcon(iconImage);
+	}
+	public static JPanel getHelperPanel(JLabel lblNaslov) {
+		JPanel helperPanel = new JPanel();
+		helperPanel.add(lblNaslov);
+		return helperPanel;
 	}
 }
