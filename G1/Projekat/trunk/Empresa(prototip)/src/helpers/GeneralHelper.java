f0ac4e5ba6f2073e2cd0d8ead861f282ed34eb84
StefanJ2022 <StefanJ2022@66b613c1-3a70-904c-bb12-c541796278d3>
2021-06-23 21:09:45 +0000

    Zavrsna verzija. Testni Dijagram.

--- G1/Projekat/trunk/Empresa(prototip)/src/helpers/GeneralHelper.java
+++ G1/Projekat/trunk/Empresa(prototip)/src/helpers/GeneralHelper.java
@@ -1,11 +1,15 @@
 package helpers;
 
+import java.awt.Component;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class GeneralHelper {
@@ -22,9 +26,21 @@ public class GeneralHelper {
 		Image iconImage = Toolkit.getDefaultToolkit().getImage("images/elements/" + iconName +  ".png");
 		return new ImageIcon(iconImage);
 	}
-	public static JPanel getHelperPanel(JLabel lblNaslov) {
+	public static JPanel getHelperPanel(Component component) {
 		JPanel helperPanel = new JPanel();
-		helperPanel.add(lblNaslov);
+		helperPanel.add(component);
 		return helperPanel;
 	}
+	public static BufferedImage getTestDiagram(String slika)
+	{
+		BufferedImage myPicture = null;
+		try {
+			myPicture = ImageIO.read(new File(slika));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return myPicture;
+	}
+	
 }
